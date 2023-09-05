9109be9d81920fc4ee24f205e1b294d10121e19a
lee <leeyeonju@iyeonjuui-MacBookPro.local>
2023-09-06 02:03:28 +0900

    프런트 단 js 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home"); //index.js 만들어준 js 파일을 
 //앱세팅 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 //던져진 라우터를 받기 위함
 //home폴더 안에 있는 index.js를 읽게 된다. ,루트라는 경로로 들어오면 home으로 보내준다
