dea532282165f01297a4aad99b47c884e3245859
lee <leeyeonju@iyeonjuui-MacBookPro.local>
2023-09-06 16:42:03 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,5 +11,12 @@ function login() {
         id: id.value,
         psword: psword.value,
     };
-    console.log(req);
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application",
+        },
+        body: JSON.stringify(req),
+    });
 }
